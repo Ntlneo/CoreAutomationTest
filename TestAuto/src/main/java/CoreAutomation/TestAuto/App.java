ee1149d749e4eadb4c9776ca249716297f917c0e
LamNguyen <lam.nguyenthanh84@gmail.com>
2019-09-25 18:00:39 +0700

    init maven project

---
+++ TestAuto/src/main/java/CoreAutomation/TestAuto/App.java
@@ -0,0 +1,13 @@
+package CoreAutomation.TestAuto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ TestAuto/src/test/java/CoreAutomation/TestAuto/AppTest.java
@@ -0,0 +1,25 @@
+package CoreAutomation.TestAuto;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+
+}
