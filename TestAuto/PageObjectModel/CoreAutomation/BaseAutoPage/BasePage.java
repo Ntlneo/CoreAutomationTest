1952c2f5e3940bf21f95ff5360ff200dfca3a15e
Lam Nguyen <lam.nguyenthanh84@gmail.com>
2020-11-06 16:31:57 +0700

    DONE add TryCLick and SwitchWindow to BasePage

--- TestAuto/PageObjectModel/CoreAutomation/BaseAutoPage/BasePage.java
+++ TestAuto/PageObjectModel/CoreAutomation/BaseAutoPage/BasePage.java
@@ -54,6 +54,24 @@ public class BasePage {
 //		jop.showMessageDialog(jf, "Please verify captcha manually then click OK", "WAITING TO VERIFY CAPTCHA",jop.INFORMATION_MESSAGE);
 	}
 	
+	public void openNewTab_thenCloseOldTabs() {
+		driver.switchTo().newWindow(WindowType.TAB);
+		String currentWindow = driver.getWindowHandle();
+//		System.out.println("CURRENT New Tab: " + currentWindow);
+		Set<String> windowSet = driver.getWindowHandles();
+		for (String window : windowSet) {
+			if(!window.equals(currentWindow)) {
+//				System.out.println("Other Window: " + window);
+				driver.switchTo().window(window).close();
+//				System.out.println("CLOSED : " + window);
+			}
+			else {							
+				driver = driver.switchTo().window(window);	
+//				System.out.println("SWITCHED New Tab : " + window);	
+			}
+		}
+	}
+	
 	public void closeAllWindow() {
 		String currentWindow = driver.getWindowHandle();
 		Set<String> windows = driver.getWindowHandles();
@@ -75,6 +93,18 @@ public class BasePage {
 			driver = driver.switchTo().window(window);
 			System.out.println(window);
 		}		
+	}
+	
+	// start from 1
+	public void switchWindow(int orderOfWindow) {
+		Set<String> windows = driver.getWindowHandles();
+		List<String> listWindow = new ArrayList<String>();
+		listWindow.addAll(windows);
+		driver = driver.switchTo().window(listWindow.get(orderOfWindow - 1));
+	}
+	
+	public int getNumberOfWindow() {
+		return driver.getWindowHandles().size();
 	}	
 	
 	static public void runCmdComand(String command) {		
@@ -99,6 +129,67 @@ public class BasePage {
 	
 	// *********************** PROTECTED Function	
 	
+	protected void tryToClickManyTime_UntilFailTextDisappear(int limitTimes, By elementToClick, int intervalInSecond, By failTextElement, String failText) {
+		int tryTimes = 0;
+		do {			
+			click(elementToClick);
+			tryTimes += 1;
+			System.out.println("TRIED CLICK: " + tryTimes);
+			try {
+				Thread.sleep(Duration.ofSeconds(intervalInSecond).toMillis());
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} while (getElement_AfterFluentWait(failTextElement, 10, 1).getText().contentEquals(failText) && tryTimes < limitTimes);
+	}
+	
+	protected void tryToClickManyTime_UntilSuccessTextAppear(int limitTimes, By elementToClick, int intervalInSecond, By successTextElement, String successText) {
+		int tryTimes = 0;
+		do {			
+			click(elementToClick);
+			tryTimes += 1;
+			System.out.println("TRIED CLICK: " + tryTimes);
+			try {
+				Thread.sleep(Duration.ofSeconds(intervalInSecond).toMillis());
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} while (!getElement_AfterFluentWait(successTextElement, 10, 1).getText().contentEquals(successText) && tryTimes < limitTimes);
+	}
+	
+	protected void tryToClickManyTime(int limitTimes, By elementToClick, int intervalInSecond) {
+		int tryTimes = 0;
+		do {			
+			click(elementToClick);
+			tryTimes += 1;
+			System.out.println("TRIED CLICK: " + tryTimes);
+			try {
+				Thread.sleep(Duration.ofSeconds(intervalInSecond).toMillis());
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} while (tryTimes < limitTimes);
+	}	
+	
+	protected void tryToClickAgain_IfFail(int limitTimes, By elementToClick, int intervalInSecond) {
+		int tryTimes = 1;
+		for (int i = 0; i < limitTimes; i++) {
+			try {
+				click(elementToClick);
+				tryTimes += i;
+				System.out.println("TRIED CLICK: " + tryTimes);
+				Thread.sleep(Duration.ofSeconds(intervalInSecond).toMillis());
+			} catch (Exception e) {
+				click(elementToClick);
+				tryTimes += 1;
+				System.out.println("TRIED CLICK: " + tryTimes);
+			}
+		}		
+	}	
+	
 	protected void executeByJavaScript(String javaScript) {
 		JavascriptExecutor js = (JavascriptExecutor)driver;  
 		js.executeScript(javaScript);		
@@ -118,18 +209,6 @@ public class BasePage {
 	protected Boolean isElementVisibleThenDisplayed(By by) {
 		return getElement_AfterFluentWait_Default(by).isDisplayed();
 	}
-	
-	// start from 1
-	protected void switchWindow(int orderOfWindow) {
-		Set<String> windows = driver.getWindowHandles();
-		List<String> listWindow = new ArrayList<String>();
-		listWindow.addAll(windows);
-		driver = driver.switchTo().window(listWindow.get(orderOfWindow - 1));
-	}
-	
-	protected int getNumberOfWindow() {
-		return driver.getWindowHandles().size();
-	}	
 	
 	protected WebElement getElement_AfterFluentWait(By by, int timeoutInSecond, int repeatInSecond) {
 		Wait wait = new FluentWait(driver).withTimeout(Duration.ofSeconds(timeoutInSecond))
